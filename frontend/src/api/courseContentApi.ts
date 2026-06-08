396eae05a71028360f5c4ae97f9656d24d304cc4
Nekibe-Bytyqi <nekibebytyqi@gmail.com>
2026-06-08 02:10:10 -0700

    Permisim i nje errori

--- frontend/src/api/courseContentApi.ts
+++ frontend/src/api/courseContentApi.ts
@@ -12,23 +12,20 @@ export const deleteSection = (id: number) =>
 
 export const updateSection = (id: number, data: any) =>
   api.put(`/course-content/sections/${id}`, data).then(r => r.data);
-
-// Lessons
+//Lessons
 export const createLesson = (data: any) =>
-    api.post("/course-content/lessons", data).then(r => r.data);
+  api.post("/course-content/lessons", data).then(r => r.data);
 
 export const deleteLesson = (id: number) =>
-  api.delete(`/course-content/lessons/${id}`);
+api.delete(`/course-content/lessons/${id}`);
 
 export const updateLesson = (id: number, data: any) =>
-  api.put(`/course-content/lessons/${id}`, data).then(r => r.data);
+api.put(`/course-content/lessons/${id}`, data).then(r => r.data);
 
 export const uploadLessonFile = (formData: FormData) =>
-  api.post(`/course-content/lessons`, formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  }).then(r => r.data);
-
-
+api.post(`/course-content/lessons`, formData, {
+  headers: { "Content-Type": "multipart/form-data" },
+}).then(r => r.data);
 
 // GET assignments by course
 // ASSIGNMENTS API
